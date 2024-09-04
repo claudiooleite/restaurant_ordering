6307b9f10628c80c38100be08cd606c6b7ae17a7
Claudio Leite <claudio.leite@hotmail.com>
2024-09-04 13:32:33 +0200

    code refactored

--- main.js
+++ main.js
@@ -1,59 +1,85 @@
 import { menuArray } from "./data.js";
 
 const container = document.getElementById("container");
-const orderDiv = document.querySelector("#order-div");
 
-// Add Items to menu
 const menuItemList = () => {
   return menuArray
     .map((item) => {
       return ` 
       <div class="container-item">
         <p class="emoji-food">${item.emoji}</p>
-        <div class="menu-item ">
+        <div class="menu-item">
           <h2>${item.name}</h2>
           <p>${item.ingredients.join(", ")}</p>
           <p>Price: $${item.price}</p>
         </div>
-        <button class="add-btn" id=${item.id}>+</button>
-      </div>
-              `;
+        <button class="add-btn" id="${item.id}">+</button>
+      </div>`;
     })
     .join("");
 };
 
+//  Add items to container
 container.innerHTML = menuItemList();
 
-// Add items to order
-const addBtn = document.getElementsByClassName("add-btn");
+//  basket to manage state of order
 
-for (let btn of addBtn) {
-  btn.addEventListener("click", addItemOrder);
+let basketItems = [];
+let totalPrice = 0;
+let counter = 0
+
+// Select btns
+const addBtns = document.getElementsByClassName("add-btn");
+
+// Method to add an item to the basket
+const addItem = (e) => {
+  const item = menuArray[e.target.id];
+  
+  
+
+  //  render only one item
+  !basketItems.includes(item.name) ? renderOrder(item) : "";
+
+  basketItems.push(item.name);
+
+  totalPrice += item.price;
+
+  // render only one total price div
+  basketItems.length === 1 ? renderTotalPrice() : "";
+
+  // updates total price
+  document.getElementById("total-price-id").textContent = `$${totalPrice}`;
+
+  // updates counter
+
+
+};
+
+for (let btn of addBtns) {
+  btn.addEventListener("click", addItem);
 }
 
-function addItemOrder(e) {
-  // Get the ID of the clicked button
-  const idItem = e.target.id;
+// Method to update the DOM
+const renderOrder = (item) => {
+  const orderDiv = document.querySelector("#order-div");
 
-  // Disable btn
-  e.target.disabled = true;
-
-  // Generate the HTML for the order item
-  const htmlOrder = `
-  <div class="order-item">
-      <h2>Your Order</h2>
-      <div>
-        <h3>${menuArray[idItem].name}</h3>
-        <button>remove</button>
-        <p>PRICE</p>
+  orderDiv.innerHTML += `
+      <div class="order-item" id="order-item-${item.id}">
+        <div>
+          <h3>${item.name}</h3>
+          <button class="remove-btn" data-id="${item.id}">remove</button>
+          <p>PRICE: $${item.price}</p>
+        </div>
       </div>
-      <h3>$${menuArray[idItem].price}</h3>
-  </div>
-  <h2>Total price:<span>PRICE</span></h2>
-<button>Complete order</button>
+    `;
+};
 
+const renderTotalPrice = () => {
+  const totalPriceElement = document.createElement("div");
+  totalPriceElement.innerHTML = `
+  <h3>Total price:<span id='total-price-id'>PRICE</span></h3>
+  <button>Complete Order</button>
   `;
-  orderDiv.innerHTML += htmlOrder;
-}
-
-//  create order html
+  const orderDiv = document.querySelector("#order-div");
+  orderDiv.insertAdjacentElement("afterend", totalPriceElement);
+};
